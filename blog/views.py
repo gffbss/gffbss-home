3f38352d43261d3d1e6ddcdc59751a9aa245ef20
Geoff Boss <gboss4@gmail.com>
2014-03-02 17:07:06 -0800

    began building homepage

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+# Create your views here.
+from django.shortcuts import render
+from blog.models import Post
+
+def home(request):
+    return render(request, "home.html")
+
+def index(request):
+    # get the blog posts that are puplished
+    posts = Post.objects.filter(published=True)
+    # return the rendered template
+    return render(request, 'blog/index.html', {'posts':posts})
+
+def post(request, slug):
+    # get the Post object
+    post = Post.objects.get(slug=slug)
+    # return the rendered template
+    return render(request, 'blog/post.html', {'post':post})
